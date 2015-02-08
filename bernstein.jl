83c5c4c5f42530b666fa74f62f3d502079eb91d4
Vishal Gupta <vishal.gupta@marshall.usc.edu>
2015-02-07 18:48:41 -0800

    cleaning up

--- bernstein.jl
+++ bernstein.jl
@@ -2,27 +2,6 @@
 # Using the benrstein type stuff
 ##########
 using JuMP
-function supp_rep(vs, phat, Gamma)
-	m = Model()
-	@defVar(m, theta)
-	@defVar(m, lam >=0)
-	@addConstraint(m, theta >= maximum(vs))
-
-	const d = length(vs)
-	#define the inner bits
-	@defVar(m, inner[1:d] >= 0)	
-	for i = 1:d
-		@addNLConstraint(m, theta - vs[i] == lam * inner[i])
-	end
-
-	@defVar(m, term)
-	@addNLConstraint(m, term <= lam * sum{phat[i] * log(inner[i]), i=1:d})
-
-	@setNLObjective(m, Min, (Gamma-1)*lam + theta - term)
-	solve(m)
-	getObjectiveValue(m), getValue(theta), getValue(lam)
-end
-
 function bernVar(vs, phat, Gamma; TOL = 1e-8, factor=10)
 	if maximum(vs) - minimum(vs) < TOL
 		println("Degenerate")
@@ -53,15 +32,3 @@ function mult_bracket(fun, val_; factor=2, max_iter=50)
 	iter == max_iter && error("Bracketing Failed")
 	(val/factor, val)
 end
-
-# mgf_p(vs, phat, lam, Gamma) = lam*Gamma - lam*dot(phat, log(1 - vs./lam))
-# deriv(vs, phat, lam, Gamma) = Gamma + dot(phat, vs./(vs-lam)) - dot(phat, log(1 - vs/lam))
-# lammin(vbars; TOL=1e-8) = max(maximum(vbars), 0) + TOL
-
-# function mgf_p(vs, phat, Gamma)
-# 	lamstar = fzero(l->deriv(vs, phat, l, Gamma), lammin(vs),  1e2)
-# 	lammin(vs), lamstar, mgf_p(vs, phat, lamstar, Gamma)
-# end
-
-
-
